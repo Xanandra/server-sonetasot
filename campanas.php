41be780b0fb45bbed8dfd0baac71ad98698114e2
Maria Villalobos <mariaelevillalcortes@gmail.com>
2023-04-09 19:01:31 -0600

    Modificacion de query Campanas

--- campanas.php
+++ campanas.php
@@ -10,7 +10,7 @@ $servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos 
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
 
-// Consultar por id
+// Consultar 
 if (isset($_GET["consultar"])){
     $sqlCampana = mysqli_query($conexionBD,"SELECT * FROM campanas WHERE id=".$_GET["consultar"]);
     if(mysqli_num_rows($sqlCampana) > 0){
@@ -20,54 +20,7 @@ if (isset($_GET["consultar"])){
     }
     else{  echo json_encode(["success"=>0]); }
 }
-//borrar
-if (isset($_GET["borrar"])){
-    $sqlCampana = mysqli_query($conexionBD,"DELETE FROM campanas WHERE id=".$_GET["borrar"]);
-    if($sqlCampana){
-        echo json_encode(["success"=>1]);
-        exit();
-    }
-    else{  echo json_encode(["success"=>0]); }
-}
-//Insertar
-if(isset($_GET["insertar"])){
-    $data = json_decode(file_get_contents("php://input"));
-    $edades=$data->edades;
-    $dosis=$data->dosis;
-    $marca=$data->marca;
-    $dateDosis=$data->dateDosis;
-    $dateAplic=$data->dateAplic;
-    $modulo=$data->modulo;
-    $domicilio=$data->domicilio;
-    $municipio=$data->municipio;
-        if(($edades!="")&&($dosis!="")&&($modulo!="")&&($domicilio!="")&&($municipio!="")){
-            
-    $sqlCampana = mysqli_query($conexionBD,"INSERT INTO campanas(edades,dosis,marca,dateDosis,dateAplic,modulo,domicilio,municipio) VALUES('$edades','$dosis','$marca','$dateDosis','$dateAplic','$modulo','$domicilio','$municipio') ");
-    echo json_encode(["success"=>1]);
-        }
-    exit();
-}
-// Actualizar
-if(isset($_GET["actualizar"])){
-    
-    $data = json_decode(file_get_contents("php://input"));
 
-    $id=(isset($data->id))?$data->id:$_GET["actualizar"];
-    $edades=$data->edades;
-    $dosis=$data->dosis;
-    $marca=$data->marca;
-    $dateDosis=$data->dateDosis;
-    $dateAplic=$data->dateAplic;
-    $modulo=$data->modulo;
-    $domicilio=$data->domicilio;
-    $municipio=$data->municipio;
-    
-    $sqlCampana = mysqli_query($conexionBD,"UPDATE campanas SET edades='$edades',dosis='$dosis',
-    marca='$marca',dateDosis='$dateDosis',dateAplic='$dateAplic',modulo='$modulo',domicilio='$domicilio',
-    municipio='$municipio' WHERE id='$id'");
-    echo json_encode(["success"=>1]);
-    exit();
-}
 // Consultar todo
 $sqlCampana = mysqli_query($conexionBD,"SELECT * FROM campanas ");
 if(mysqli_num_rows($sqlCampana) > 0){
